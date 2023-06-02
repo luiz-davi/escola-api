7f2a127143d348b609b82625fdae5256a76397a9
luiz-davi-capyba <luizd4398@gmail.com>
2023-06-02 15:28:30 -0300

    usando views genéricas

--- curso/urls.py
+++ curso/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import CursoAPIView, AvaliacaoAPIView
+from . import views
+
 
 urlpatterns = [
-    path('cursos/', CursoAPIView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes')
+    path('cursos/', views.CursosAPIView.as_view(), name='cursos'),
+    path('cursos/<int:pk>', views.CursoAPIView.as_view(), name='curso'),
+    path('avaliacoes/', views.AvaliacoesAPIView.as_view(), name='avaliacoes'),
+    path('avaliacoes/<int:pk>', views.AvaliacaoAPIView.as_view(), name='avaliacao'),
 ]
