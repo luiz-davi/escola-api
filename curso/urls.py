3640ef65b7ce61472c73eb160990ceb77c9275ed
luiz-davi-capyba <luizd4398@gmail.com>
2023-06-03 01:37:49 -0300

    sobrescrevendo métodos de listagem se visualização + adicação de novos endpoints

--- curso/urls.py
+++ curso/urls.py
@@ -5,7 +5,12 @@ from . import views
 
 urlpatterns = [
     path('cursos/', views.CursosAPIView.as_view(), name='cursos'),
-    path('cursos/<int:pk>', views.CursoAPIView.as_view(), name='curso'),
+    path('cursos/<int:pk>/', views.CursoAPIView.as_view(), name='curso'),
+    path('cursos/<int:curso_pk>/avaliacoes/',
+         views.AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/',
+         views.AvaliacaoAPIView.as_view(), name='curso_avaliacao'),
     path('avaliacoes/', views.AvaliacoesAPIView.as_view(), name='avaliacoes'),
-    path('avaliacoes/<int:pk>', views.AvaliacaoAPIView.as_view(), name='avaliacao'),
+    path('avaliacoes/<int:avaliacao_pk>/',
+         views.AvaliacaoAPIView.as_view(), name='avaliacao'),
 ]
